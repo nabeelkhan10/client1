668ab2cd81266adcc0f888db5481e6485690a74b
nabeel khan <31417655+nabeelkhan10@users.noreply.github.com>
2021-10-02 19:38:05 +0530

    Update website.js

--- website.js
+++ website.js
@@ -1,11 +1,9 @@
 const currentlocation = location.href;
-const menuitem = document.querySelectorAll('nav-list a');
-const menulenght = menuitem.length;
+const menuitem = document.querySelectorAll( '.nav-list a' );
 
-for(let i = 0; i < menulenght; i++){
-    if(menuitem[i].href === currentlocation){
-        menuitem[i].classList.add('active');
-        menuitem[i].classList.remove('navlink');
+for ( let i = 0; i < menuitem.length; i++ ) {
+    if ( menuitem[i].href === currentlocation ) {
+        menuitem[i].classList.add( "active" )
     }
 }
 
